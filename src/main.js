a11aa9bace93560d343b49162d3634b82249881c
teityou <510350964@qq.com>
2022-07-13 11:52:28 +0800

    项目初始化

---
+++ src/main.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+// 第三方 css库  让不同的标签在不同的浏览器中样式统一
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 国际化 语言包
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+// 全局公共样式
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import store from './store'
+import router from './router'
+
+import '@/icons' // icon
+import '@/permission' // permission control  权限控制
+
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online ! ! !
+ */
+// if (process.env.NODE_ENV === 'production') {
+//   const { mockXHR } = require('../mock')
+//   mockXHR()
+// }
+
+// set ElementUI lang to EN
+Vue.use(ElementUI, { locale })
+// 如果想要中文版 element-ui，按如下方式声明
+// Vue.use(ElementUI)
+
+Vue.config.productionTip = false // 控制台有一个生产的提示
+// 初始化vue 挂载 路由 vuex
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
